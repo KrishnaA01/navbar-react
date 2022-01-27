da078bc392ce89b4684d06252328d91885b135e9
KrishnaA01 <mahato77@hotmail.com>
2022-01-27 14:35:04 +0100

    conditional rendering

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,45 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [showLinks,setShowLinks] = useState(false)
+
+
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle" onClick={()=>{
+            setShowLinks(!showLinks)
+          }}>
+            <FaBars />
+          </button>
+        </div>
+        {
+        showLinks && <div className="links-container show-container">
+          <ul className="links">
+           {links.map((link)=>{
+             const {id,url,text} = link
+             return <li key={id}>
+               <a href={url}>{text}</a>
+             </li>
+           })}
+          </ul>
+        </div>
+        }
+        {/* conditional rendering if showLinks is true then show things after && */}
+        <ul className="social-icons">
+        {social.map((socialIcon)=>{
+          const {id,url,icon} = socialIcon
+          return <li key={id}>
+            <a href={url} > {icon} </a>
+          </li>
+        })}
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
